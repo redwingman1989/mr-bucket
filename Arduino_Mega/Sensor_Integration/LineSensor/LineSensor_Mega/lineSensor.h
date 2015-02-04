7f1d333976103778793fe77138b1266544da7f00
redwingman1989@gmail.com <redwingman1989@gmail.com@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-02-04 05:18:15 +0000

    Line Sensor Mega. Works without using Timer3 Interrupt.
    To Do:
    1) Figure out why Timer3 Interrupt "clears" Line Sensor data.

---
+++ Arduino_Mega/Sensor_Integration/LineSensor/LineSensor_Mega/lineSensor.h
@@ -0,0 +1,16 @@
+#ifndef _LINESENSOR_H
+#define _LINESENSOR_H
+
+#define LINE_DETECTION_TIME_IN_US (800)
+
+unsigned long startCapChargeTime;
+unsigned long frontLineSensorDischargeTimes[NUM_LINE_SENSOR_SENSORS];
+
+unsigned char sensorDetectsWhite[NUM_LINE_SENSOR_SENSORS]; // Make this a bitfield
+
+unsigned char portA;
+
+
+
+
+#endif
---
+++ Arduino_Mega/Sensor_Integration/LineSensor/LineSensor_Mega/lineSensor_c.ino
@@ -0,0 +1,68 @@
+#include "lineSensor.h"
+
+void lineSensorExec() {
+  startCapChargeTime = 0; // Reset the cap charge times
+  memset(frontLineSensorDischargeTimes, 0, NUM_LINE_SENSOR_SENSORS * sizeof(unsigned long)); // reset discharge time
+  
+  // Drive Sensor Line High
+//  digitalWrite(FRONT_LN_SEN_SENSOR_1_PIN, HIGH);
+//  digitalWrite(FRONT_LN_SEN_SENSOR_2_PIN, HIGH);
+    // Direct Port Access
+    PORTC = PORTC | B11111111;
+  
+  
+  // Make the port an output
+//  pinMode(FRONT_LN_SEN_SENSOR_1_PIN, OUTPUT);
+//  pinMode(FRONT_LN_SEN_SENSOR_2_PIN, OUTPUT);
+    // Direct Port Access
+    DDRC = DDRC | B11111111;
+    
+  // Charge the lines for 10 us
+  delayMicroseconds(10);
+  
+  // Make the port an input
+//  pinMode(FRONT_LN_SEN_SENSOR_1_PIN, INPUT);
+//  pinMode(FRONT_LN_SEN_SENSOR_2_PIN, INPUT);
+    // Direct Port Access
+    DDRC = DDRC & B00000000;
+    
+  // Mark the time of the start of charging the caps
+  startCapChargeTime = micros();
+  
+  //delayMicroseconds(150);
+  
+//  pollLineSensor();
+  
+  // Start the Interrupt Timer that will poll the Line Sensor
+  Timer3.start();
+}
+
+
+
+
+
+
+
+void pollLineSensor() {
+  /* For each of the sensors on the line sensor, check to see if the capacitor has discharged. */
+  for (unsigned char sensorNumOnLineSensor = 0; sensorNumOnLineSensor < NUM_LINE_SENSOR_SENSORS; sensorNumOnLineSensor++) {
+    /* Line Sensor is on PORT C. Get the value of PORT  and mask each sensor slot to see if the sensor
+     *   sees a white surface. After the Timer Interrupt triggers, the bits representing PORT C will be
+     *   a value of 1 or 0.
+     *   0 = White Surface 
+     *   1 = Black/Dark Surface */
+    if (((PINC & (1 << sensorNumOnLineSensor)) == FALSE)) {
+      /* If the capacitor discharged, PORT C input channel will be 0. Mark the amount of time that it took. 
+       *   If the time value is 0 after leaving this ISR, the Line Sensor's Sensor is over a dark or black
+       *   surface. */
+      frontLineSensorDischargeTimes[sensorNumOnLineSensor] = micros() - startCapChargeTime;
+    } /* If (PINC...) */
+  } /* For loop */
+  
+  if (DEBUG_BUILD == TRUE) {
+    printOutLineSenData = TRUE;
+    portA = PINC;
+  } /* if(Debug) */
+  
+  //Timer3.stop();
+}
