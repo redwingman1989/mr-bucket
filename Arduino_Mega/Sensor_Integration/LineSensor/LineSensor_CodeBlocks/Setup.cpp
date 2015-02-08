1829354647cc45b33768525216b6f4da22e474f5
redwingman1989@gmail.com <redwingman1989@gmail.com@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-02-08 02:43:57 +0000

    Codeblocks Project.

---
+++ Arduino_Mega/Sensor_Integration/LineSensor/LineSensor_CodeBlocks/Setup.cpp
@@ -0,0 +1,63 @@
+#include "Setup.h"
+#include "Arduino.h"
+#include "TimerOne.h"
+
+/*******************************************************************
+ Function: void setupTimer1Int(void)
+ Description: Initializes the main PIT Interrupt Timer. The Timer
+                expires after periodInMicroSecs microseconds. When
+                the time expires the PIT function is called. The
+                timer may be configured using the
+                PIT_PERIOD_IN_MICROSECS macro, located in global
+                When the timer expires the PIT fuction is called as
+                the ISR.
+*******************************************************************/
+void setupTimer1Int(unsigned long periodInMicroSecs) {
+  Timer1.initialize(periodInMicroSecs);
+  Timer1.attachInterrupt(PIT);
+}
+
+/*******************************************************************
+ Function: void setupTimer3Int(void)
+ Description: Initializes the Line Sensor Polling Interrupt Timer.
+                The Polling interrupt will be enabled and disabled
+                during code execution. It will only be enabled after
+                the capacitors have been charged. After the line is
+                found the polling interrupt is disabled. The timer
+                may be configured using the POLL_PERIOD_IN_MICROSECS
+                macro, located in global.h
+*******************************************************************/
+void setupTimer3Int(unsigned long periodInMicroSecs) {
+//  Timer3.initialize(periodInMicroSecs);
+//  Timer3.attachInterrupt(pollLineSensor);
+//  Timer3.stop();
+
+}
+
+/*******************************************************************
+ Function: void setupSerial(void)
+ Description: Initializes the main debug serial port. The port is
+                initialized to 9600 baud.
+*******************************************************************/
+void setupSerial(unsigned long baud) {
+  Serial.begin(baud);
+}
+
+/*******************************************************************
+ Function: void setupLineSensor(void)
+ Description: Initializes the robot's Line Sensor. Set the LED Enable
+                Digital I/O to an output. Set the LED Enable pin High.
+*******************************************************************/
+void setupLineSensor() {
+  pinMode(ALL_LN_SEN_LED_ENABLE_PIN, OUTPUT);
+  digitalWrite(ALL_LN_SEN_LED_ENABLE_PIN, HIGH);
+}
+
+
+/*******************************************************************
+ Function: void setupPinModes(void)
+ Description: Initializes Arduino Mega Digital I/O pins.
+*******************************************************************/
+void setupPinModes() {
+  pinMode(LED_PIN, OUTPUT);
+}
