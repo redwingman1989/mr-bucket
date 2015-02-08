d1a68b8aa006381216741027c6957ba76419d553
Matt.R.Fleck <Matt.R.Fleck@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-02-08 21:48:45 +0000

    Magnetometer code

---
+++ trunk/Arduino_Mega/Magno/Magno.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include "Magnetometer.h"
+
+/* create global MAgnetomer with I2C address 0x1E */
+Magnetometer mag (0x1E);
+
+void setup(){
+  Serial.begin(57600);
+  mag.init();
+}
+
+void loop(){
+  delay(20);
+  Serial.println(mag.getHeading());
+}
+
