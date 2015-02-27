8f576147e03b0f9aab925b0ca64150cc1368efa6
redwingman1989@gmail.com <redwingman1989@gmail.com@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-02-27 09:36:29 +0000

    This code compiles, but is fucking broken.

--- trunk/Arduino_Mega/UltraSonic/Interrupts.h
+++ trunk/Arduino_Mega/UltraSonic/Interrupts.h
@@ -13,22 +13,21 @@
 /* Enumeration */
 enum externalInterrupts {
     EXT_INTERRUPT_0,
+    /* External Interrupt 0 = Right UltraSonic Sensor */
+    ULTRA_RIGHT_EXT_INTERRUPT_NUM = EXT_INTERRUPT_0,
 
     EXT_INTERRUPT_1,
 
-    /* External Interrupt 2 = Left UltraSonic Sensor */
     EXT_INTERRUPT_2,
-    ULTRA_LEFT_EXT_INTERRUPT_NUM = EXT_INTERRUPT_2,
 
-    /* External Interrupt 3 = Front UltraSonic Sensor */
     EXT_INTERRUPT_3,
-    //ULTRA_FRONT_EXT_INTERRUPT_NUM = EXT_INTERRUPT_3,
 
-    /* External Interrupt 4 = Right UltraSonic Sensor */
     EXT_INTERRUPT_4,
-    ULTRA_RIGHT_EXT_INTERRUPT_NUM = EXT_INTERRUPT_4,
+    /* External Interrupt 4 = Left UltraSonic Sensor */
+    ULTRA_LEFT_EXT_INTERRUPT_NUM = EXT_INTERRUPT_4,
 
     EXT_INTERRUPT_5,
+    /* External Interrupt 5 = Front UltraSonic Sensor */
     ULTRA_FRONT_EXT_INTERRUPT_NUM = EXT_INTERRUPT_5,
 
     EXT_INTERRUPT_6,
