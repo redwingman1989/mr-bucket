e4f197bf914e0dd477cdf3dba320dd10cfeff12a
Matt.R.Fleck <Matt.R.Fleck@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-02-28 02:17:29 +0000

    State Machine stuff is working

--- Robot/Sonic/MainExecMachine.cpp
+++ Robot/Sonic/MainExecMachine.cpp
@@ -1,25 +1,27 @@
 #include "MainExecMachine.h"
+#include "StateMachine.h"
 #include "Globals.h"
 
 MainExecMachine::MainExecMachine() {
-  currentState = (state) &MainExecMachine::load;
+  currentState = (state) &MainExecMachine::loadRings;
   stateNum = MEST_LOAD_LR_RINGS;
 }
 
-
-void MainExecMachine::load(bool firstTime) {
+void MainExecMachine::loadRings(bool firstTime) {
   static bool buttonsDetected = 0;
   float distanceToFront = ultraSonicMgr.getSensor(FRONT)->getCalculatedDistanceValue();
   /* Move Forward Till we hit Buttons */
 
+  Serial.print("Hade It Here 1\n");
   /* Buttons Detected */
-  if (!buttonsDetected && ((buttMan.getButtons() & 0x03) && distanceToFront < 1)) {
+  if (!buttonsDetected && (buttMan.getButtons() & 0x03) && (distanceToFront < 1)) {
+      Serial.print("Hade It Here 2\n");
     timeOut = micros();
     buttonsDetected = true;
-    wheels.KillMotors();
+    wheels.updateCommand(0,0,0);
   }
   else {
-    wheels.updateCommand(15,0,0);
+    wheels.updateCommand(5,0,0);
   }
 
   if (buttonsDetected) {
@@ -41,28 +43,29 @@ void MainExecMachine::load(bool firstTime) {
         desiredHeading = currentHeading - 180;
         break;
     }
-  }
 
-  /* We waited long enough, change state to back up */
-  if (micros() - timeOut > ringLoadTime) {
-    currentState = (state) &MainExecMachine::backUp;
-    buttonsDetected = false;
+
+    /* We waited long enough, change state to back up */
+    if (micros() - timeOut > ringLoadTime) {
+      currentState = (state) &MainExecMachine::backUp;
+      buttonsDetected = false;
+    }
   }
 }
 
 void MainExecMachine::backUp(bool firstTime) {
   bool exit = 0;
-
+  Serial.println("im in backup");
   /* if we just transistioned to this state*/
 
-  wheels.updateCommand(-15,0,0);
+  wheels.updateCommand(-5,0,0);
 
   switch (stateNum) {
     case MEST_BACKUP_ONE:
       if (lineManager.getLineDriveCommand(LSP_BACK).valid ||
           ultraSonicMgr.getSensor(FRONT)->getCalculatedDistanceValue() > 7.5)
       {
-        wheels.KillMotors();
+        wheels.updateCommand(0,0,0);
         stateNum = MEST_SHIFT_FOR_CENTER;
         currentState = (state) &MainExecMachine::shiftForCenter;
       }
@@ -70,7 +73,7 @@ void MainExecMachine::backUp(bool firstTime) {
     case MEST_BACKUP_TWO:
       if (ultraSonicMgr.getSensor(FRONT)->getCalculatedDistanceValue() > 16)
       {
-        wheels.KillMotors();
+        wheels.updateCommand(0,0,0);
         stateNum = MEST_FLIP_ONE;
         currentState = (state) &MainExecMachine::flipABitch;
       }
@@ -78,7 +81,7 @@ void MainExecMachine::backUp(bool firstTime) {
     case MEST_BACKUP_THREE:
       if (ultraSonicMgr.getSensor(FRONT)->getCalculatedDistanceValue() > 16)
       {
-        wheels.KillMotors();
+        wheels.updateCommand(0,0,0);
         stateNum = MEST_FLIP_TWO;
         currentState = (state) &MainExecMachine::flipABitch;
       }
@@ -104,19 +107,37 @@ void MainExecMachine::shiftForCenter(bool firstTime) {
 
   if (centerSensorCount && rightSensorCount &&
       lineManager.getLineDriveCommand(LSP_CENTER).valid) {
-    wheels.KillMotors();
+    wheels.updateCommand(0,0,0);
     stateNum = MEST_LOAD_CENTER_RINGS;
-    currentState = (state) &MainExecMachine::load;
+    currentState = (state) &MainExecMachine::loadRings;
     centerSensorCount = false;
     rightSensorCount = false;
   }
 }
 
 void MainExecMachine::flipABitch(bool firstTime) {
-  //
+  int16_t heading = mag.getRawHead();
+  int16_t delta;
+  static int16_t lastDelta = 0;
+  int16_t deltaDelta;
 
-  // rotate till current heading is within tollerance of desired heading
+  delta = desiredHeading - heading;
+  if (delta > 180)
+    delta -= 360;
+  if (delta < -180)
+    delta += 360;
+  deltaDelta = delta - lastDelta;
 
+    if (abs(delta) <= 3 && abs(deltaDelta) < 1) {
+      wheels.updateCommand(0,0,0);
+      stateNum = MEST_FIND_CENTER_LINE_ONE;
+      currentState = (state) &MainExecMachine::findCenterLine;
+    }
+    else {
+        if (delta>0) wheels.updateCommand(0,0,(0.2*delta));
+        else wheels.updateCommand(0,0,(0.2*delta));
+    }
+    lastDelta = delta;
   // once the desired heading is reached
   //   transition to findCenterLin
 }
@@ -124,9 +145,15 @@ void MainExecMachine::flipABitch(bool firstTime) {
 void MainExecMachine::findCenterLine(bool firstTime) {
   // Check Ultrasonic Sensors and Magnetometer to move to the center line
 
+
   // If found centerline on Right Line Sensors, transition to haulAss
 }
 
 void MainExecMachine::haulAss(bool firstTime) {
 
+}
+
+void MainExecMachine::DebugOutput(HardwareSerial * serialPort){
+  serialPort->print("serialNum: ");
+  serialPort->println(stateNum);
 }
