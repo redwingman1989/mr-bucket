85a9d4311699f73417b0f240490efe5549250436
the1bbb <the1bbb@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-03-03 03:27:42 +0000

    Added drive code that resembles the better days. Constants may need to be tweaked.

--- trunk/Robot/Sonic/driveAlgorithms.h
+++ trunk/Robot/Sonic/driveAlgorithms.h
@@ -7,35 +7,28 @@ float convertRadToPercent(float rad, float speed){
 }
 
 void FollowLine(float speedx,float speedy, lineSensorPairs linePairEnum){
-    static float xLast = 0;
-    static float yLast = 0;
 
     lineDriveCommand_t linePair = lineManager.getLineDriveCommand(linePairEnum);
     float adjustedAngleRad = linePair.angle - pairAngleOffset[linePairEnum];
     adjustedAngleRad = convertRadToPercent(adjustedAngleRad,max(abs(speedx),abs(speedy)));
-    float xDistance = - 1 *(linePair.offset.x - pairCenters[linePairEnum].x );
-    float yDistance = - 1 *(linePair.offset.y - pairCenters[linePairEnum].y );
+
     float speedConst = 15 * .25;
-    float deltaX = xLast - xDistance;
-    deltaX = abs(deltaX * speedConst);
-    float deltaY = yLast - yDistance;
-    deltaY = abs(deltaY * speedConst);
-    float speedConstX = speedConst * abs(xDistance);
-    float speedConstY = speedConst * abs(yDistance);
-
-
-    if(xDistance > 0)
-        xDistance = speedConstX  - min(deltaX,speedConstX);
-    else
-        xDistance = -1 * speedConstX + min(deltaX,speedConstX);
-    if(yDistance > 0)
-        yDistance = speedConstY - min(deltaY,speedConstY);
-    else
-        yDistance = -1 * speedConstY + min(deltaY,speedConstY);
-
-    wheels.updateCommand(speedy + yDistance,speedx + xDistance,adjustedAngleRad);
-    xLast = xDistance;
-    yLast = yLast;
+    float speedConstCenter = 1;
+    float centerThreshhold = .7;
+    float speed = 0;
+    if(speedy > speedx){
+       speed = - 1 *(linePair.offset.x - pairCenters[linePairEnum].x );
+       if(abs(speed) > centerThreshhold)
+        wheels.updateCommand(speedy ,speedConst ,adjustedAngleRad);
+       else
+        wheels.updateCommand(speedy ,speed * speedConstCenter ,adjustedAngleRad);
+    } else{
+       speed = - 1 *(linePair.offset.y - pairCenters[linePairEnum].y );
+       if(abs(speed) > centerThreshhold)
+        wheels.updateCommand(speedConst , speedx ,adjustedAngleRad);
+       else
+         wheels.updateCommand(speed * speedConstCenter , speedx ,adjustedAngleRad);
+    }
 }
 
 float getToHeading(float desiredHeading) {
