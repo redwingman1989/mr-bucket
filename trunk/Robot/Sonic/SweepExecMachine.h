700d71a0bc413ab52dcfa6055e9f943042f2f042
Matt.R.Fleck <Matt.R.Fleck@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-03-22 20:49:51 +0000

    Further Framework setup for the sweeping sub state machine. ready to populate.

--- trunk/Robot/Sonic/SweepExecMachine.h
+++ trunk/Robot/Sonic/SweepExecMachine.h
@@ -5,8 +5,33 @@
 typedef enum {
   Z_LOAD,
   Z_SCORE,
+
   Z_NUM_OF_ZONES
 } Zone;
+
+typedef enum {
+  LZ_BACK_UP_INITIAL,
+  LZ_SHIFT_LEFT_TO_WALL,
+  LZ_DRIVE_FORWARD,
+  LZ_SHIFT_RIGHT_TO_WALL,
+  LZ_BACK_OFF_LEFT,
+  LZ_BACK_UP_TO_ALIGN,
+  LZ_REALIGN,
+
+  LZ_NUM_OF_LZ_STATES
+} LzState;
+
+typedef enum {
+  SZ_BACK_UP_INITIAL,
+  SZ_SHIFT_RIGHT_TO_WALL,
+  SZ_DRIVE_FORWARD,
+  SZ_SHIFT_LEFT_TO_WALL,
+  SZ_BACK_OFF_RIGHT,
+  SZ_BACK_UP_TO_ALIGN,
+  SZ_REALIGN,
+
+  SZ_NUM_OF_SZ_STATES
+} SzState;
 
 class SweepExecMachine: public StateMachine<SweepExecMachine> {
   public:
@@ -15,5 +40,31 @@ class SweepExecMachine: public StateMachine<SweepExecMachine> {
     bool RunTick();
 
   private:
+    bool doneSweeping;
+    state lzStateArray[];
+    state szStateArray[];
+
+    LzState lzState;
+    SzState szState;
+
+    /* Sweep Load Zone Functions */
+    void lz_backUpInitial(bool);
+    void lz_shiftLeftToWall(bool);
+    void lz_driveForward(bool);
+    void lz_shiftRightToWall(bool);
+    void lz_backOffLeft(bool);
+    void lz_backUpToAlign(bool);
+    void lz_reAlign(bool);
+
+    /* Sweep Score Zone Functions */
+    void sz_backUpInitial(bool);
+    void sz_shiftRightToWall(bool);
+    void sz_driveForward(bool);
+    void sz_shiftLeftToWall(bool);
+    void sz_backOffRight(bool);
+    void sz_backUpToAlign(bool);
+    void sz_reAlign(bool);
 
  };
+
+
