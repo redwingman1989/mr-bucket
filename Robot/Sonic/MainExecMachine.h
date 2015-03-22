389443681b8b78be63e2a802d4dd26dc6cb2f245
Christopher.R.Kirby@gmail.com <Christopher.R.Kirby@gmail.com@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-03-22 06:36:21 +0000

    added states to separate button detection and pickup/unload

--- Robot/Sonic/MainExecMachine.h
+++ Robot/Sonic/MainExecMachine.h
@@ -3,21 +3,24 @@
 #include "StateMachine.h"
 
 typedef enum {
-  MEST_LOAD_LR_RINGS, //0
-  MEST_BACKUP_ONE, //1
-  MEST_SHIFT_FOR_CENTER, //2
-  MEST_LOAD_CENTER_RINGS, //3
-  MEST_BACKUP_TWO, //4
-  MEST_RAISE_ARM, //5
-  MEST_FLIP_ONE, //6
-  MEST_FIND_CENTER_LINE_ONE, //7
-  MEST_HAUL_TOSCORE, //8
-  MEST_SCORE, //9
-  MEST_BACKUP_THREE, //10
-  MEST_LOWER_ARM, //11
-  MEST_FLIP_TWO, //12
-  MEST_FIND_CENTER_LINE_TWO, //13
-  MEST_HAUL_TOLOAD, //14
+  MEST_LOAD_LR_RINGS,         //0
+  MEST_PICKUP_LR,             //1
+  MEST_BACKUP_ONE,            //2
+  MEST_SHIFT_FOR_CENTER,      //3
+  MEST_LOAD_CENTER_RINGS,     //4
+  MEST_PICKUP_CENTER,         //5
+  MEST_BACKUP_TWO,            //6
+  MEST_RAISE_ARM,             //7
+  MEST_FLIP_ONE,              //8
+  MEST_FIND_CENTER_LINE_ONE,  //9
+  MEST_HAUL_TOSCORE,          //10
+  MEST_SCORE,                 //11
+  MEST_UNLOAD_ALL,            //12
+  MEST_BACKUP_THREE,          //13
+  MEST_LOWER_ARM,             //14
+  MEST_FLIP_TWO,              //15
+  MEST_FIND_CENTER_LINE_TWO,  //16
+  MEST_HAUL_TOLOAD,           //17
 
   MEST_NUM_STATES,
 } StateNum;
@@ -40,15 +43,18 @@ class MainExecMachine: public StateMachine<MainExecMachine> {
 
     /* States */
     void loadLeftRightRings(bool);
+    void pickupLeftRightRings(bool);
     void backupFromLeftRightRings(bool);
     void shiftForCenterRings(bool);
     void loadCenterRings(bool);
+    void pickupCenterRings(bool);
     void backupFromCenterRings(bool);
     void raiseArm(bool);
     void flipToScore(bool);
     void findCenterLineToScore(bool);
     void haulToScore(bool);
     void scoreRings(bool);
+    void unloadAllRings(bool);
     void backupFromScoring(bool);
     void lowerArm(bool);
     void flipToLoad(bool);
