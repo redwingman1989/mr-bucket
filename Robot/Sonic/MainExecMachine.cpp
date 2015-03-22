bbb76603ee76e847a684fb3885a6b1cdf27a6853
Christopher.R.Kirby@gmail.com <Christopher.R.Kirby@gmail.com@732b1dac-6b5b-0bce-1585-7acf4e2957f0>
2015-03-22 01:23:15 +0000

    ported backup and shiftForCenterRings

--- Robot/Sonic/MainExecMachine.cpp
+++ Robot/Sonic/MainExecMachine.cpp
@@ -19,11 +19,53 @@ void MainExecMachine::loadLeftRightRings(bool first) {
 }
 
 void MainExecMachine::backupFromLeftRightRings(bool first) {
-
+  wheels.updateCommand(-2, 0, 0);
+  FollowLine(0, -2, LSP_RIGHT);
+  if (lineManager.getLineDriveCommand(LSP_BACK).valid ||
+      ultraSonicMgr.getSensor(FRONT)->getCalculatedDistanceValue() > 5)
+  {
+    wheels.updateCommand(0,0,0);
+    stateNum = MEST_SHIFT_FOR_CENTER;
+    currentState = (state) &MainExecMachine::shiftForCenterRings;
+  }
 }
 
 void MainExecMachine::shiftForCenterRings(bool first) {
+  static bool centerSensorCount = false;
+  static bool rightSensorCount = false;
+  static bool needToCenter = false;
+  static int lineUpCount = 0;
 
+  wheels.updateCommand(0, 3, 0);
+
+  if(lineManager.getLineDriveCommand(LSP_CENTER).valid) {
+    centerSensorCount = true;
+  }
+
+  if(centerSensorCount && lineManager.getLineDriveCommand(LSP_RIGHT).valid) {
+    rightSensorCount = true;
+  }
+
+  if (centerSensorCount && rightSensorCount &&
+      lineManager.getLineDriveCommand(LSP_CENTER).valid) {
+    needToCenter = true;
+  }
+
+  if (needToCenter) {
+    if(FollowLineSingle(0,true,LSL_CENTER_FRONT)) {
+            if(lineUpCount++ > 10){
+                wheels.updateCommand(0,0,0);
+                stateNum = MEST_LOAD_CENTER_RINGS;
+                currentState = (state) &MainExecMachine::loadCenterRings;
+                centerSensorCount = false;
+                rightSensorCount = false;
+                needToCenter = false;
+                lineUpCount=0;
+            }
+    } else{
+        lineUpCount = 0;
+    }
+  }
 }
 
 void MainExecMachine::loadCenterRings(bool first) {
@@ -31,11 +73,19 @@ void MainExecMachine::loadCenterRings(bool first) {
 }
 
 void MainExecMachine::backupFromCenterRings(bool first) {
-
+  wheels.updateCommand(-20, 0, 0);
+  if (ultraSonicMgr.getSensor(FRONT)->getCalculatedDistanceValue() > 16)
+  {
+    wheels.updateCommand(0,0,0);
+    stateNum = MEST_RAISE_ARM;
+    currentState = (state) &MainExecMachine::raiseArm;
+  }
 }
 
 void MainExecMachine::raiseArm(bool first) {
-
+  arm.commandSwingArm(SA_UP);
+  stateNum = MEST_FLIP_ONE;
+  currentState = (state) &MainExecMachine::flipToScore;
 }
 
 void MainExecMachine::flipToScore(bool first) {
@@ -70,11 +120,19 @@ void MainExecMachine::scoreRings(bool first) {
 }
 
 void MainExecMachine::backupFromScoring(bool first) {
-
+  wheels.updateCommand(-20, 0, 0);
+  if (ultraSonicMgr.getSensor(FRONT)->getCalculatedDistanceValue() > 16)
+  {
+    wheels.updateCommand(0,0,0);
+    stateNum = MEST_LOWER_ARM;
+    currentState = (state) &MainExecMachine::lowerArm;
+  }
 }
 
 void MainExecMachine::lowerArm(bool first) {
-
+  arm.commandSwingArm(SA_DOWN);
+  stateNum = MEST_FLIP_TWO;
+  currentState = (state) &MainExecMachine::flipToLoad;
 }
 
 void MainExecMachine::flipToLoad(bool first) {
